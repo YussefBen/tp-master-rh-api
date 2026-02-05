dcda41aefc2f9188fd2ac82730594d061ea5e88f
BEN CHOUCHANE Youssef <yussef.ben@email.com>
2026-02-05 13:24:59 +0100

    Final: TP Terminé avec succès (Login, Stats, Export CSV)

--- proxy.ts
+++ proxy.ts
@@ -4,20 +4,16 @@ import { jwtVerify } from 'jose';
 
 const SECRET = new TextEncoder().encode('MON_SUPER_SECRET');
 
-export async function middleware(req: NextRequest) {
-  // 1. Récupération du token
+export async function proxy(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
 
-  // 2. Si pas de token, on rejette
   if (!token) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
   try {
-    // 3. Vérification du token
     const { payload } = await jwtVerify(token, SECRET);
 
-    // 4. Protection Admin pour la route /api/stats
     if (req.nextUrl.pathname.startsWith('/api/stats') && !payload.isAdmin) {
       return NextResponse.json({ error: 'Forbidden: Admins only' }, { status: 403 });
     }
@@ -28,7 +24,6 @@ export async function middleware(req: NextRequest) {
   }
 }
 
-// Appliquer uniquement sur les routes API protégées
 export const config = {
   matcher: ['/api/stats/:path*', '/api/employees/:path*', '/api/export/:path*'], 
 };
